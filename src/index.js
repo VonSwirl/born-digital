2e479b884844aae6e79d70d744db0f0a8a067f7b
vonswirl <vonswirl@googlemail.com>
2020-03-15 11:12:19 +0000

    source corrected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.js'
+import App from './app.js'
 
 ReactDOM.render(<App/>, document.getElementById('app'))
